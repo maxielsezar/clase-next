dfc0cbee564270a99818fd661a575baeb71bc54e
tacs69yolo-source <tacs69yolo@gmail.com>
2025-10-20 19:04:00 -0300

    se agrego Batman

---
+++ src/app/Batman/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <p>
+        Batman ​ «en español: Hombre Murciélago» ​ es un superhéroe ficticio de cómic creado por los estadounidenses Bob Kane y Bill Finger, ​ y propiedad de DC Comics. Apareció por primera vez en la historia titulada «El caso del sindicato químico» de la revista Detective Comics N.º 27, lanzada por la editorial National Publications el 30 de marzo de 1939.
+      </p>
+      <img src="https://tse1.mm.bing.net/th/id/OIP.-k02uHC8Py1MtgHv5FmO9QHaDt?cb=12&rs=1&pid=ImgDetMain&o=7&rm=3" alt="" />
+    </div>
+  )
+}
+
+export default page
