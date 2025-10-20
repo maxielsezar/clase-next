0b9f26915003d2cf3fc81f5fabc358e180ca99bf
maxielsezar <maxielsezar@gmail.com>
2025-10-20 18:33:08 -0300

    se creo la branch dev

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="flex justify-between">
       pagina principal
+      FELIZ LUNES
     <Link href="/tami">ir a ver a tami</Link>
    </div>
   );
