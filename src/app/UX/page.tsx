4649360aef8323fb05c7fc9a98c312702a0668e4
naticayul <naticayul@gmail.com>
2025-10-21 21:55:51 -0300

    armado de header

---
+++ src/app/UX/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '@/app/componentes/Header'
+
+
+
+const page = () => {
+  return (
+    <div>
+      <Header/>
+      <p> Bolsa de empleo </p>
+    </div>
+
+  )
+}
+
+export default page
