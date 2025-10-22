4147f4dd3d993afd726e688592b23f80a85f79f9
gabrielarganaraz16-cpu <gabrielarganaraz16@gmail.com>
2025-10-21 21:54:21 -0300

    se sube ux3

---
+++ src/app/empleos/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+
+const page = () => {
+  return (
+    <div className='ml-40 h-30 bg-center m-20 bg-no-repeat bg-[url(https://cfp655esquel.edu.ar/cfp655/wp-content/uploads/2016/04/CFP-N%C2%BA-655-Blanco.png)]'>
+
+
+    </div>
+  )
+}
+
+export default page
