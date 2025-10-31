5db5e15e829e0d4eed0ec921f28c5b2a81e2b479
naticayul <naticayul@gmail.com>
2025-10-30 21:50:49 -0300

    router put y delete

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -9,6 +9,12 @@ router.get('/', async (req, res) => {
   res.json(usuarios)
 });
 
+router.get('/:id', async (req, res) => {
+  const usuarios = await Usuario.findById(req.params.id);
+  res.json(usuarios)
+});
+
+
 router.post('/',async function(req, res, next) {
   const {nombre, apellido} = req.body
   const nuevoUsuario= await Usuario.create({nombre, apellido})
@@ -19,9 +25,18 @@ router.put('/', function(req, res, next) {
   res.send('update user');
 });
 
+router.put('/:id', async (req, res) => {
+  const usuarioActualizado = await Usuario.findByIdAndUpdate(req.params.id, req.body, {new:true,});
+  res.json(usuarioActualizado)
+});
+
 router.delete('/', function(req, res, next) {
   res.send('delete user');
 });
 
+router.delete('/:id', async (req, res) => {
+  const usuarios = await Usuario.findByIdAndDelete (req.params.id);
+  res.json({mensaje: "usuario eliminado"})
+});
 
 module.exports = router;
