a69abbb0f019e91669bd03824053f2827bb0e208
Maxi <81635117+maxielsezar@users.noreply.github.com>
2025-11-04 18:53:12 -0300

    links api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import Link from "next/link";
 
+//https://bolsa-de-empleo-cfp.vercel.app/api/jobs
+//https://bolsa-de-empleo-cfp.vercel.app/api/companies
+//https://bolsa-de-empleo-cfp.vercel.app/api/applicants
+
 export default function Home() {
   return (
     <div className="flex justify-between">
