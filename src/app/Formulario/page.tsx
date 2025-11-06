f3ee6426f4875994e228785b808f464d4a20f302
gabrielarganaraz16-cpu <gabrielarganaraz16@gmail.com>
2025-11-05 21:45:21 -0300

    nada terminado ni empesado jaja

---
+++ src/app/Formulario/page.tsx
@@ -0,0 +1,38 @@
+"use client"
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+
+
+const page = () => {
+
+
+
+
+  return (
+    
+
+<>
+
+
+
+<div className='pt-10 bg-neutral-500'>
+    <div className='ml-5 h-30 bg-center m-5 bg-no-repeat bg-[url(https://cfp655esquel.edu.ar/cfp655/wp-content/uploads/2016/04/CFP-N%C2%BA-655-Blanco.png)]'></div>
+    <p className='ml-50 mt-20 text-4xl text-white'><b>TE AYUDAMOS A FORJARTE PARA EL TRABAJO EN EL :</b></p>
+</div>
+
+</>
+      
+  )
+
+  
+
+  
+  
+}
+
+
+
+
+export default page
