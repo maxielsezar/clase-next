4788e2d07bb94ab686addc432f3b4b5635ca784f
gabrielarganaraz16-cpu <gabrielarganaraz16@gmail.com>
2025-11-18 21:37:24 -0300

    hey

--- src/app/Bolsacv/page.tsx
+++ src/app/Bolsacv/page.tsx
@@ -1,17 +1,12 @@
 "use client"
 import React from "react"
-import Input from "@/app/componentes/Styled-Components2";
+import Form from "@/app/componentes/Styled-Components2";
 
 const page = () => {
 
 return(
 
-
 <>
-
-
-
-
 
 <div className=" bg-linear-to-r/hsl from-indigo-500 to-teal-400 text-6xl p-8 flex justify-center">BOLSA DE EMPLEO</div>
 <br />
@@ -20,13 +15,21 @@ return(
 <div className="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% p-4 text-3xl w-50">Postulate</div>
 <br />
 
-<div className="text-black flex justify-center text-3xl">Crea tu curriculum vitae y de esta manera quedas <br /> postulado para ofertas laborales presentes y futuras</div>
+<div className=" flex justify-center text-3xl">
+    <h3 className="text-white  bg-black w-max">
+        Crea tu curriculum vitae y de esta manera quedas 
+        <br /> 
+        postulado para ofertas laborales presentes y futuras
+    </h3>
+    
+</div>
+
 <br />
 
 <div className="bg-blue-600"></div>
 
 
-    <div className=""><Input/></div>
+    <div className="flex justify-center"><Form/> </div>
 
 
 
--- src/app/componentes/Styled-Components2.tsx
+++ src/app/componentes/Styled-Components2.tsx
@@ -1,51 +1,241 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Input = () => {
+const Form = () => {
   return (
     <StyledWrapper>
-      <div className="input-group">
-        <input required type="text" name="text" autoComplete="off" className="input" />
-        <label className="user-label">Nombre</label>
-      </div>
+      <form className="form">
+        <div className="flex">
+          <label>
+            <input required placeholder type="text" className="input" />
+            <span>Nombre</span>
+          </label>
+          <label>
+            <input required placeholder type="text" className="input" />
+            <span>Apellido</span>
+          </label>
+        </div>  
+        <label>
+          <input required placeholder type="email" className="input" />
+          <span>Correo Electronico</span>
+        </label> 
+        <label>
+          <input required type="tel" placeholder className="input" />
+          <span>Numero de Contacto</span>
+        </label>
+        <label>
+          <textarea required rows={4} placeholder className="input01" defaultValue={""} />
+          <span>Describenos tu/s objetivo/s, experiencias, etc.</span>
+        </label>
+        <button className="fancy" href="#">
+          <span className="top-key" />
+          <span className="text">Enviar</span>
+          <span className="bottom-key-1" />
+          <span className="bottom-key-2" />
+        </button>
+      </form>
     </StyledWrapper>
   );
 }
 
 const StyledWrapper = styled.div`
-  .input-group {
-   position: relative;
+  .form {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    max-width: 350px;
+    background-color: #fff;
+    padding: 20px;
+    border-radius: 10px;
+    position: relative;
   }
 
-  .input {
-   border: solid 1.5px #020024;
-   border-radius: 1rem;
-   background: none;
-   padding: 1rem;
-   font-size: 1rem;
-   color: #020024;
-   transition: border 150ms cubic-bezier(0.4,0,0.2,1);
+  .message {
+    color: rgba(88, 87, 87, 0.822);
+    font-size: 14px;
   }
 
-  .user-label {
-   position: absolute;
-   left: 15px;
-   color: #9E9E9E;
-   pointer-events: none;
-   transform: translateY(1rem);
-   transition: 150ms cubic-bezier(0.4,0,0.2,1);
+  .flex {
+    display: flex;
+    width: 100%;
+    gap: 6px;
   }
 
-  .input:focus, input:valid {
-   outline: none;
-   border: 1.5px solid #1a73e8;
+  .form label {
+    position: relative;
   }
 
-  .input:focus ~ label, input:valid ~ label {
-   transform: translateY(-50%) scale(0.8);
-   background-color: #212121;
-   padding: 0 .2em;
-   color: #2196f3;
+  .form label .input {
+    width: 100%;
+    padding: 10px 10px 20px 10px;
+    outline: 0;
+    border: 1px solid rgba(105, 105, 105, 0.397);
+    border-radius: 5px;
+  }
+
+  .form label .input + span {
+    position: absolute;
+    left: 10px;
+    top: 15px;
+    color: grey;
+    font-size: 0.9em;
+    cursor: text;
+    transition: 0.3s ease;
+  }
+
+  .form label .input:placeholder-shown + span {
+    top: 15px;
+    font-size: 0.9em;
+  }
+
+  .form label .input:focus + span,.form label .input:valid + span {
+    top: 30px;
+    font-size: 0.7em;
+    font-weight: 600;
+  }
+
+  .form label .input:valid + span {
+    color: green;
+  }
+
+  .input01 {
+    width: 100%;
+    padding: 10px 10px 20px 10px;
+    outline: 0;
+    border: 1px solid rgba(105, 105, 105, 0.397);
+    border-radius: 5px;
+  }
+
+  .form label .input01 + span {
+    position: absolute;
+    left: 10px;
+    top: 10px;
+    color: grey;
+    font-size: 0.9em;
+    cursor: text;
+    transition: 0.3s ease;
+  }
+
+  .form label .input01:placeholder-shown + span {
+    top: 40px;
+    font-size: 0.9em;
+  }
+
+  .form label .input01:focus + span,.form label .input01:valid + span {
+    top: 50px;
+    font-size: 0.7em;
+    font-weight: 600;
+  }
+
+  .form label .input01:valid + span {
+    color: green;
+  }
+
+  .fancy {
+    background-color: transparent;
+    border: 2px solid #cacaca;
+    border-radius: 0px;
+    box-sizing: border-box;
+    color: #fff;
+    cursor: pointer;
+    display: inline-block;
+    font-weight: 390;
+    letter-spacing: 2px;
+    margin: 0;
+    outline: none;
+    overflow: visible;
+    padding: 8px 30px;
+    position: relative;
+    text-align: center;
+    text-decoration: none;
+    text-transform: none;
+    transition: all 0.3s ease-in-out;
+    user-select: none;
+    font-size: 13px;
+  }
+
+  .fancy::before {
+    content: " ";
+    width: 1.7rem;
+    height: 2px;
+    background: #cacaca;
+    top: 50%;
+    left: 1.5em;
+    position: absolute;
+    transform: translateY(-50%);
+    transform: translateX(230%);
+    transform-origin: center;
+    transition: background 0.3s linear, width 0.3s linear;
+  }
+
+  .fancy .text {
+    font-size: 1.125em;
+    line-height: 1.33333em;
+    padding-left: 2em;
+    display: block;
+    text-align: left;
+    transition: all 0.3s ease-in-out;
+    text-transform: lowercase;
+    text-decoration: none;
+    color: #818181;
+    transform: translateX(30%);
+  }
+
+  .fancy .top-key {
+    height: 2px;
+    width: 1.5625rem;
+    top: -2px;
+    left: 0.625rem;
+    position: absolute;
+    background: white;
+    transition: width 0.5s ease-out, left 0.3s ease-out;
+  }
+
+  .fancy .bottom-key-1 {
+    height: 2px;
+    width: 1.5625rem;
+    right: 1.875rem;
+    bottom: -2px;
+    position: absolute;
+    background: white;
+    transition: width 0.5s ease-out, right 0.3s ease-out;
+  }
+
+  .fancy .bottom-key-2 {
+    height: 2px;
+    width: 0.625rem;
+    right: 0.625rem;
+    bottom: -2px;
+    position: absolute;
+    background: white;
+    transition: width 0.5s ease-out, right 0.3s ease-out;
+  }
+
+  .fancy:hover {
+    color: white;
+    background: #cacaca;
+  }
+
+  .fancy:hover::before {
+    width: 1.5rem;
+    background: white;
+  }
+
+  .fancy:hover .text {
+    color: white;
+    padding-left: 1.5em;
+  }
+
+  .fancy:hover .top-key {
+    left: -2px;
+    width: 0px;
+  }
+
+  .fancy:hover .bottom-key-1,
+   .fancy:hover .bottom-key-2 {
+    right: 0;
+    width: 0;
   }`;
 
-export default Input;
+export default Form;
+
