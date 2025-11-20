b49c5990334111f11bd5457085b5a0b9d6c60d41
gabrielarganaraz16-cpu <gabrielarganaraz16@gmail.com>
2025-11-19 21:50:09 -0300

    hey

--- src/app/Bolsacv/page.tsx
+++ src/app/Bolsacv/page.tsx
@@ -8,14 +8,14 @@ return(
 
 <>
 
-<div className=" bg-linear-to-r/hsl from-indigo-500 to-teal-400 text-6xl p-8 flex justify-center">BOLSA DE EMPLEO</div>
+<div className="bg-linear-to-r/hsl from-indigo-500 to-teal-400 text-6xl p-8 flex justify-center">BOLSA DE EMPLEO</div>
 <br />
 <br />
 
 <div className="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% p-4 text-3xl w-50">Postulate</div>
 <br />
 
-<div className=" flex justify-center text-3xl">
+<div className="flex justify-center text-3xl">
     <h3 className="text-white  bg-black w-max">
         Crea tu curriculum vitae y de esta manera quedas 
         <br /> 
@@ -26,10 +26,28 @@ return(
 
 <br />
 
-<div className="bg-blue-600"></div>
-
-
     <div className="flex justify-center"><Form/> </div>
+    <br />
+
+
+    <div className="bg-linear-to-r/hsl from-indigo-500 to-teal-400 p-6 flex justify-center">Aqui hay un instituto donde puedes realizar cursos
+    <br />
+    <br />
+    <a href="https://cfp655esquel.edu.ar/cfp655/"><img src={"/CFP-Nº-655-Blanco.png"}></img></a>
+     
+
+    <div className="ml-70">
+
+        
+        <ul>
+            <li>Sede Chacabuco 1036 – Esquel – Chubut</li>
+            <li>Tel / Fax: 2945 453449</li>
+            <li>
+                <a href="mailto:centro655@cfp655esquel.edu.ar">Mail: centro655@cfp655esquel.edu.ar</a>
+                </li>
+        </ul>
+    </div>
+    </div>
 
 
 
