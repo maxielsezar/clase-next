e4d2c192b4cb1d1851f22daa67dc2b62473b656f
Román11 <romanruminahuel26549@gmail.com>
2025-11-26 18:57:12 -0300

    cambios

--- src/app/componentes/Upload.tsx
+++ src/app/componentes/Upload.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const Upload = () => {
   return (
     <StyledWrapper>
-      <div className="container"> 
+      <div className="justify-center"> 
         <div className="header"> 
           <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth={0} /><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" /><g id="SVGRepo_iconCarrier"> 
               <path d="M7 10V9C7 6.23858 9.23858 4 12 4C14.7614 4 17 6.23858 17 9V10C19.2091 10 21 11.7909 21 14C21 15.4806 20.1956 16.8084 19 17.5M7 10C4.79086 10 3 11.7909 3 14C3 15.4806 3.8044 16.8084 5 17.5M7 10C7.43285 10 7.84965 10.0688 8.24006 10.1959M12 12V21M12 12L15 15M12 12L9 15" stroke="#000000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" /> </g></svg> <p>Explorar archivos para subir</p>
@@ -21,19 +21,7 @@ const Upload = () => {
 }
 
 const StyledWrapper = styled.div`
-  .container {
-    height: 300px;
-    width: 300px;
-    border-radius: 10px;
-    box-shadow: 4px 4px 30px rgba(0, 0, 0, .2);
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-between;
-    padding: 10px;
-    gap: 5px;
-    background-color: rgba(0, 110, 255, 0.041);
-  }
+  
 
   .header {
     flex: 1;
