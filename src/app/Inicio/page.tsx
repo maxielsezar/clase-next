495f185a07077ca6b1e244846905dfa35e2fc30f
tomiletelier <tomiiletelier@gmail.com>
2025-11-26 20:54:50 -0300

    nuevos cambios

--- src/app/Inicio/page.tsx
+++ src/app/Inicio/page.tsx
@@ -1,36 +1,7 @@
-'use client'
-import { useState, useEffect } from 'react'
-import { IoEyeSharp } from "react-icons/io5";
-import { FaEyeSlash } from "react-icons/fa"; 
-import axios from 'axios';
+import InicioSesion from '../componentes/InicioSesion';
 import './contenido.css'
 
 const page = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState(''); 
-  
-      const [data,setData]= useState('')
-  
-      useEffect(() => { 
-      async function getUser() {
-        try {
-        const response = await axios.get('/user?ID=12345');
-        console.log(response);
-        } catch (error) {
-        console.error(error);
-  };
-      getUser(); }
-      }, []); 
-
-
-
-  const handleGoogleSignIn = () => {
-    console.log('Iniciando sesión con Google');
-  }
-  const [verPassword, setVerPassword] = useState(false); 
-
-    const handleSubmit = (e: React.FormEvent) => {
-        console.log('Iniciando sesión'); }
 
 return ( 
   <>
@@ -38,25 +9,9 @@ return (
       <img className='logo1' src="/CFP-Nº-655-Blanco.png" />
       <h1>CENTRO DE FORMACIÓN Nº655</h1>
     </div>
-    <div className='contenido'>
-      <h2>Iniciar Sesión</h2>
-        <form onSubmit={handleSubmit}>
-        <label htmlFor='email'>Correo electrónico:</label>
-        <input type="email" value={email} onChange={(e)=> setEmail(e.target.value)}/>
-        <label htmlFor='password'>Contraseña:</label>
-        <div>
-          <input className="w-130" type={verPassword ? 'password' : 'text'} value={password} onChange={(e) => setPassword(e.target.value)}/>
-          <button onClick={() => setVerPassword(!verPassword)}>
-            {verPassword ? <IoEyeSharp /> : <FaEyeSlash />}
-          </button>
-        </div>
-        <button type="submit">Iniciar Sesión</button>
-        <button className='google' onClick={handleGoogleSignIn}>Iniciar sesión con Google</button>  
-        </form>
-        <div className="olvido"><a href="/reset">¿Olvidaste tu contraseña?</a></div>
-        <p className="texto">¿No tenés cuenta? <a href="/register">Regístrate</a></p>
-    </div>  
+    <InicioSesion />
   </>   
     );     
 };
+
 export default page;
